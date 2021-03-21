20ad56df1f62f5f51a3533c1635f4faf50ee683a
Ivan Andreev <ceasar1@rambler.ru>
2021-03-21 19:40:56 +0300

    colorpicker title

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -3,7 +3,7 @@
     <li class="colors__item" v-for="color in colors" :key="color.id">
       <label class="colors__label">
         <input class="colors__radio sr-only" type="radio" :value="color.id" v-model="pickerColor">
-        <span class="colors__value" :style="{'background-color': color.value, 'border': '1px solid lightgrey'}">
+        <span class="colors__value" :style="{'background-color': color.value, 'border': '1px solid lightgrey'}" :title="color.title">
         </span>
       </label>
     </li>
